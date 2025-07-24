5c7b280d24f90394aef34c46c3fba22e107e8f19
djytwy <676534074@qq.com>
2025-07-24 12:03:02 +0800

    update: add eslint v9

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,14 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  webpack: (config) => {
-    // v1:
-    config.externals = [...config.externals, 'canvas', 'jsdom'];
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    return config;
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  images: {
-    domains: ['ambrus.s3.amazonaws.com', 'cdn.ambrus.studio', 'storage.googleapis.com'],
-  },
-  typescript: {
-    // v1: !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
   eslint: {
-    // v1:
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  // output: 'export',
-  // images: {
-  //   unoptimized: true,
-  // },
-  env: {
-    AESKey: 'chihyen!@&$,&handsome',
-  },
-};
+  reactStrictMode: true,
+  cleanDistDir: true,
+}
 
-export default nextConfig;
+export default nextConfig
